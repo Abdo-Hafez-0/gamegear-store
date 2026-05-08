79c8ad009e95f286d38f33a43ea32f735989220d
Abdo Hafez <abdohafez731@gmail.com>
2026-05-08 17:51:32 +0300

    feat: initial backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "GameGear API Running"
+  });
+});
+
+module.exports = app;
