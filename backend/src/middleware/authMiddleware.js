1253769185b708d61cbb9e4f135e7455a29fe52e
Abdo Hafez <abdohafez731@gmail.com>
2026-05-09 16:25:59 +0300

    feat: added middleware auth

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,48 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  try {
+
+    let token;
+
+    // Check Authorization Header
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+
+      // Extract Token
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify Token
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET
+      );
+
+      // Find User
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+
+    } else {
+
+      return res.status(401).json({
+        message: "Not authorized, no token",
+      });
+
+    }
+
+  } catch (error) {
+
+    return res.status(401).json({
+      message: "Not authorized, invalid token",
+    });
+
+  }
+};
+
+module.exports = {
+  protect,
+};
