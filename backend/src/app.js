03d550b9a6d8f6167e2ae5edb79845d930bcca2d
Abdo Hafez <abdohafez731@gmail.com>
2026-05-09 16:28:19 +0300

    feat: register all the middlewares in the app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 
+const {
+  notFound,
+  errorHandler,
+} = require("./middleware/errorMiddleware");
+
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Test Route
-app.get("/", (req, res) => {
-  res.json({
-    message: "GameGear API Running"
-  });
-});
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Error Middleware
+app.use(notFound);
+app.use(errorHandler);
+
 
 module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,10 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 5000;
-
-// Connect Database
 connectDB();
 
-// Start Server
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
